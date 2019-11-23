61b770da8e94277d0edd415eada239b952f8f8c4
Leriche Pierre <pierre.leriche@epitech.eu>
2019-11-23 18:52:13 +0100

    feat (connection) : Particle connection ready

--- website/index.js
+++ website/index.js
@@ -1,11 +1,15 @@
-// server.js
+// index.js
 
 // set up ======================================================================
 // get all the tools we need
 var express  = require('express');
+var bodyParser   = require('body-parser');
+
 var app      = express();
 var port     = process.env.PORT || 8080;
 
+app.use(bodyParser.json()); // get information from html forms
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs'); // set up ejs for templating
 
 
--- website/routes/routes.js
+++ website/routes/routes.js
@@ -1,13 +1,46 @@
+var Particle = require('particle-api-js');
+var particle = new Particle();
+let token;
+
 module.exports = function(app) {
 
     // normal routes ===============================================================
 
         // show the home page (will also have our login links)
         app.get('/', function(req, res) {
+            console.log("index get");
             res.render('index.ejs');
         });
 
         app.get('/login', function(req, res) {
+            console.log("login get");
             res.render('login.ejs');
+        });
+
+        app.post('/login', function(req, res) {
+            console.log("login post");
+            particle.login({username: req.body['email'], password: req.body['password']}).then(
+                function(data) {
+                    token = data.body.access_token;
+                    res.render('player.ejs');
+                },
+                function (err) {
+                    console.log('Could not log in.', err);
+                    res.render('login.ejs');
+                }
+            );
+        });
+
+        app.post('/lightItUp', function(req, res) {
+            var fnPr = particle.callFunction({ deviceId: '510047001851353530333932', name: 'lightItUp', argument: '', auth: token });
+
+            fnPr.then(
+            function(data) {
+                console.log('Function called succesfully:', data);
+            }, function(err) {
+                console.log('An error occurred:', err);
+            });
+
+            res.render('player.ejs');
         });
 }
